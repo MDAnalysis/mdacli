5eb89db20e9ee5216f9405a8f9875a67e7110ef8
joaomcteixeira <joaomcteixeira@gmail.com>
2021-01-26 09:31:38 -0500

    Adding CI and repository base files
    Some details still need to be reviewed.

---
+++ docs/AUTHORS.rst
@@ -0,0 +1,5 @@
+Authors
+=======
+
+* Philip Lecko (`webpage <>`_, `github <https://github.com/PicoCentauri>`_)
+* Joao M. C. Teixeira (`webpage <https://bit.ly/joaomcteixeira>`_, `github <https://github.com/joaomcteixeira>`_)
---
+++ setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""Setup dot py."""
+from __future__ import absolute_import, print_function
+
+# import re
+from glob import glob
+from os.path import basename, dirname, join, splitext
+
+from setuptools import find_packages, setup
+
+
+def read(*names, **kwargs):
+    """Read description files."""
+    path = join(dirname(__file__), *names)
+    with open(path, encoding=kwargs.get('encoding', 'utf8')) as fh:
+        return fh.read()
+
+long_description = '{}\n{}'.format(
+    read('README.rst'),
+    read(join('docs', 'CHANGELOG.rst')),
+    )
+
+setup(
+    name='mdacli',
+    version='0.0.0',
+    description='A command line client for MDAnalysis Analysis classes.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license='MIT License',
+    author='',
+    author_email='',
+    maintainer='',
+    maintainer_email='',
+    url='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(i))[0] for i in glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        ],
+    project_urls={
+        'webpage': '',
+        'Documentation': '',
+        'Changelog': '',
+        'Issue Tracker': '',
+        'Discussion Forum': '',
+        },
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+        ],
+    python_requires='>=3.6, <3.9',
+    install_requires=[
+        # 'click',
+        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        ],
+    extras_require={
+        # eg:
+        #   'rst': ['docutils>=0.11'],
+        #   ':python_version=="2.6"': ['argparse'],
+        },
+    setup_requires=[
+        #   'pytest-runner',
+        #   'setuptools_scm>=3.3.1',
+        ],
+    entry_points={
+        'console_scripts': [
+            'mdacli= mdacli.cli:main',
+            ]
+        #
+        },
+    # cmdclass={'build_ext': optional_build_ext},
+    # ext_modules=[
+    #    Extension(
+    #        splitext(relpath(path, 'src').replace(os.sep, '.'))[0],
+    #        sources=[path],
+    #        include_dirs=[dirname(path)]
+    #    )
+    #    for root, _, _ in os.walk('src')
+    #    for path in glob(join(root, '*.c'))
+    # ],
+    )
