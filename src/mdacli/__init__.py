a58311ab28c5df04cfaa7f5175b9d99482922c11
Philip Loche <ploche@physik.fu-berlin.de>
2021-05-12 11:30:56 +0200

    Bumped year

--- src/mdacli/__init__.py
+++ src/mdacli/__init__.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+#
+# Copyright (c) 2021 Authors and contributors
+#
+# Released under the GNU Public Licence, v2 or any higher version
+# SPDX-License-Identifier: GPL-2.0-or-later
 """Command line interfaces for MDAnalysis analysis classes."""
 
 from .cli import maincli, setup_clients
