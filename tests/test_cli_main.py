59cf692766e62faa92fa71f5e6de90347b96f2d4
Philip Loche <ploche@physik.fu-berlin.de>
2021-05-19 13:10:40 +0200

    Reworked tests

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -8,10 +8,8 @@
 """Test mdacli."""
 
 import pytest
-from MDAnalysis.analysis.rdf import InterRDF
-from MDAnalysis.tests.datafiles import DCD, PSF
 
-from mdacli.cli import analyze_data, convert_str_time
+from mdacli.cli import convert_str_time
 
 
 @pytest.mark.parametrize('x, frame',
@@ -34,25 +32,3 @@ def test_convert_str_time_raise():
     """Test convert string to time ValueError."""
     with pytest.raises(ValueError):
         convert_str_time('0.1', dt=1)
-
-
-class Test_analyze_data(object):
-    """Test class for analyze_data."""
-
-    @pytest.fixture()
-    def kwargs(self):
-        """Keyword arguments for run."""
-        kwargs = {}
-        kwargs["begin"] = "0"
-        kwargs["end"] = "1"
-        kwargs["dt"] = "1"
-        kwargs["verbose"] = False
-        kwargs["g1"] = "all"
-        kwargs["g2"] = "all"
-        kwargs["func"] = None
-        return kwargs
-
-    def test_run(self, kwargs, tmpdir):
-        """Simple test run."""
-        with tmpdir.as_cwd():
-            analyze_data(PSF, DCD, analysis_callable=InterRDF, **kwargs)
---
+++ tests/test_save.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+#
+# Copyright (c) 2021 Authors and contributors
+#
+# Released under the GNU Public Licence, v2 or any higher version
+# SPDX-License-Identifier: GPL-2.0-or-later
+"""Test mdacli save."""
+
+import sys
+from unittest.mock import patch
+
+import mdacli.save
+
+
+def test_get_cli_input():
+    """Test the cli input format."""
+    testargs = ['maicos', 'foo', "foo bar"]
+    with patch.object(sys, 'argv', testargs):
+        cli_in = 'Command line was: maicos foo "foo bar"'
+        assert mdacli.save.get_cli_input() == cli_in
