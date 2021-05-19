5de210106f91048eb17c6a013e9e9b9f8f8570cd
joaomcteixeira <joaomcteixeira@gmail.com>
2021-05-19 18:03:36 +0200

    Improves save_results
    Improves docstring
    General improvements cleaning
    Saves ndim > 3 to npz
    
    TODO:
    - some refact on save_results
    - some testing

--- src/mdacli/save.py
+++ src/mdacli/save.py
@@ -5,6 +5,7 @@
 #
 # Released under the GNU Public Licence, v2 or any higher version
 # SPDX-License-Identifier: GPL-2.0-or-later
+"""Manage data saving."""
 import json
 import os
 import sys
@@ -14,7 +15,135 @@ import numpy as np
 
 from MDAnalysis.analysis.base import Results
 
-"""Manage data saving."""
+from mdacli.colors import Emphasise
+
+
+def save_results(fprefix, results):
+    """
+    Save the attributes of a results instance to disk.
+
+    1D, 2D and 3D numpy arrays are saved to csv files. 1D arrays of the
+    same length are vertically stacked to create a table. 2D arrays are
+    saved directly. 3D arrays are split into 2D arrays along the
+    shortest dimension and one CSV is saved for each 2D array created,
+    and resulting CSVs are stored together in a ZIP file. Note: higher
+    dimensional arrays are ignored.
+
+    We try to save everything else in a JSON file. Non-serializable
+    types are ignored.
+
+    Parameters
+    ----------
+    fprefix : str
+        prefix for all files saved
+
+    results : `~MDAnalysis.analysis.base.Results`
+        A Results instance from which the stored data is taken.
+    """
+    list_1D = []
+    list_1D_labels = []
+    json_dict = {}
+
+    for key, item in results.items():
+        if isinstance(item, Results):
+            # Run `save_results` recursively if
+            # `item` is results instancee
+            save_results(f"{fprefix}_{key}", item)
+        elif isinstance(item, np.ndarray):
+            # Remove extra dimensions
+            item = np.squeeze(item)
+            n_dims = item.ndim
+
+            if n_dims == 1:
+                list_1D.append(item)
+                list_1D_labels.append(key)
+
+            elif n_dims == 2:
+                np.savetxt(
+                    fname=f"{fprefix}_{key}.csv",
+                    X=item,
+                    fmt='%-10s',
+                    #delimiter='\t',
+                    )
+
+            elif n_dims == 3:
+                min_dim = np.argmin(item)
+                files_to_zip = []
+
+                # Split array along the dimension with smallest number
+                # of entries
+                splitted_item = np.split(
+                    item,
+                    item.shape[min_dim],
+                    axis=min_dim,
+                    )
+
+                for arr in splitted_item:
+                    files_to_zip.append(f"{key}_dim_{min_dim}_idx_{i}.csv")
+                    np.savetxt(
+                        fname=files_to_zip[-1],
+                        X=np.squeeze(arr),
+                        delimiter=',',
+                        )
+
+                # Compress all csv files into a single zip archive
+                with zipfile.ZipFile(f'{key}.zip', 'w') as zipF:
+                    for file_name in files_to_zip:
+                        zipF.write(
+                            file_name,
+                            compress_type=zipfile.ZIP_DEFLATED,
+                            )
+                        os.remove(file_name)
+
+            elif n_dims > 3:
+                np.save(
+                    f"{fprefix}_{key}",
+                    item,
+                    allow_pickle=False,
+                    )
+
+            else:
+                warnings.warn(
+                    Emphasise.warning(
+                        "Saving numpy arrays with more than "
+                        "three dimensions is currently not supported."
+                        )
+                    )
+
+        elif (isinstance(item, (bool, int, float, list, tuple, dict))
+              or item is None):
+            # This can be encoded in a json file
+            json_dict[key] = item
+
+        else:
+            warnings.warn(Emphasise.warning(f"Saving {key} of type {type(item)}"
+                          "is currently not supported."))
+
+    # Stack 1D arrays and save teheem to csv
+    if len(list_1D) > 0:
+        out_lists, out_lables = stack_1d_arrays_list(list_1D, list_1D_labels)
+
+        command_run = ' '.join(sys.argv) + "\n"
+
+        for out_list, out_label in zip(out_lists, out_lables):
+            out_label = out_label.flatten()
+
+            # [3:] to align lables with entries
+            savetxt_w_command(
+                fname=f"{fprefix}_{'_'.join(out_label)}.csv",
+                X=out_list.T,
+                header=''.join([f"{i:>25}" for i in out_label])[3:]
+                )
+
+    # Save everything which is left to a json file
+    # if json dict is not empty
+    if json_dict:
+        with open(f'{fprefix}.json', 'w') as f:
+            json.dump(json_dict, f)
+
+    return
+
+
 def stack_1d_arrays_list(list_1D, extra_list=None):
     """Stack a list of 1D numpy arrays of the same length vertically together.
 
@@ -68,97 +197,6 @@ def stack_1d_arrays_list(list_1D, extra_list=None):
         return out_lists
 
 
-def save_results(fprefix, results):
-    """Save the attributes of a results instance to disk.
-
-    1D, 2D and 3D numpy arrays are saved to csv files. All 1D arrays
-    of the same lengths are veertically stacked. For 3D arrays
-    a csv file is created for the dimension with the lowest number of
-    indices. Higher dimensional arrays are ignored.
-
-    Everything else is tried to saved inside a json file. Types which
-    can not be saved into json are ignored.
-
-    Parameters
-    ----------
-    fprefix : str
-        prefix for all files saved
-    results : `MDAnalysis.analysis.base.Results`
-        A Results instance from which the stored data is taken.
-    """
-    list_1D = []
-    list_1D_labels = []
-    json_dict = {}
-
-    for key, item in results.items():
-        if isinstance(item, Results):
-            # Run `save_results` recursively if
-            # `item` is results instancee
-            save_results(f"{fprefix}_{key}", item)
-        elif isinstance(item, np.ndarray):
-            # Remove extra dimensions
-            item = np.squeeze(item)
-            n_dims = len(item.shape)
-
-            if n_dims == 1:
-                list_1D.append(item)
-                list_1D_labels.append(key)
-            elif n_dims == 2:
-                np.savetxt(fname=f"{fprefix}_{key}.csv",
-                           X=item,
-                           delimiter=',')
-            elif n_dims == 3:
-                min_dim = np.argmin(item)
-                files_to_zip = []
-                # Split array along the dimension with smallest number
-                # of entries
-                for i, arr in enumerate(np.split(item,
-                                                 item.shape[min_dim],
-                                                 axis=min_dim)):
-                    files_to_zip.append(f"{key}_dim_{min_dim}_idx_{i}.csv")
-                    np.savetxt(fname=files_to_zip[i],
-                               X=np.squeeze(arr),
-                               delimiter=',')
-
-                # Compress all csv files into a single zip archive
-                with zipfile.ZipFile(f'{key}.zip', 'w') as zipF:
-                    for file in files_to_zip:
-                        zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
-                        os.remove(file)
-
-            else:
-                warnings.warn("Saving numpy arrays with more than "
-                              "three dimensions is currently not supported.")
-        elif (isinstance(item, (bool, int, float, list, tuple, dict))
-              or item is None):
-            # This can be encoded in a json file
-            json_dict[key] = item
-
-        else:
-            warnings.warn(f"Saving {key} of type {type(item)}"
-                          "is currently not supported.")
-
-    # Stack 1D arrays and save teheem to csv
-    if len(list_1D) > 0:
-        out_lists, out_lables = stack_1d_arrays_list(list_1D, list_1D_labels)
-
-        command_run = ' '.join(sys.argv) + "\n"
-
-        for out_list, out_label in zip(out_lists, out_lables):
-            out_label = out_label.flatten()
-
-            # [3:] to align lables with entries
-            savetxt_w_command(
-                fname=f"{fprefix}_{'_'.join(out_label)}.csv",
-                X=out_list.T,
-                header=''.join([f"{i:>25}" for i in out_label])[3:]
-                )
-
-    # Save everything which is left to a json file
-    # if json dict is not empty
-    if json_dict:
-        with open(f'{fprefix}.json', 'w') as f:
-            json.dump(json_dict, f)
 
 
 def savetxt_w_command(fname, X, header='', fsuffix=".csv", **kwargs):
