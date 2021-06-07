280b9b14f2b25e650cf187a29f4dccf1df9a640b
Philip Loche <ploche@physik.fu-berlin.de>
2021-06-07 13:04:05 +0200

    Fixed tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -38,19 +38,19 @@ _relev_mod = list(_relevant_modules)
 def test_required_args():
     """Test that there is a module given."""
     with pytest.raises(subprocess.CalledProcessError):
-        subprocess.check_call(['mdacli'])
+        subprocess.check_call(['mda'])
 
 
 def test_wrong_module():
     """Test for a non existent module."""
     with pytest.raises(subprocess.CalledProcessError):
-        subprocess.check_call(['mdacli', 'foo'])
+        subprocess.check_call(['mda', 'foo'])
 
 
 @pytest.mark.parametrize('args', ("version", "help"))
 def test_extra_options(args):
     """Test for a ab extra option."""
-    subprocess.check_call(['mdacli', '--' + args])
+    subprocess.check_call(['mda', '--' + args])
 
 
 @pytest.mark.parametrize(
