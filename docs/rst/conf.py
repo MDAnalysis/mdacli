c5af55bde3c4d4f946ce5dc9ab438bbd6d3a12a7
joaomcteixeira <joaomcteixeira@gmail.com>
2021-10-20 19:22:33 +0200

    correct bumpversion in files

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -49,7 +49,7 @@ project = 'mdacli'
 year = '2021'
 author = 'Philip Lecko and Joao MC Teixeira'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.0'
+version = release = '0.1.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
