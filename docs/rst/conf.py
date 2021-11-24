b989042fbd4898786e456c4a7963edf2175045a6
joaomcteixeira <joaomcteixeira@users.noreply.github.com>
2021-11-24 19:09:06 +0000

    [SKIP] version bump 0.1.3 -> 0.1.4
    Update links (#75)
    
    Update links to new domain mdacli.mdanalysis.org

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -61,7 +61,7 @@ project = 'mdacli'
 year = '2021'
 author = 'Philip Loche, Joao MC Teixeira and Oliver Beckstein'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.3'
+version = release = '0.1.4'
 
 pygments_style = 'trac'
 templates_path = ['_templates']
