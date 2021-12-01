22b2fa6d561647bd9347b6d5662da372dbbed5bf
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2021-12-01 05:29:04 -0800

    Deploy docs (#78)
    Deploy docs to mdanalysis.org
    
    Co-authored-by: Philip Loche <philip-loche@gmx.de>

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -6,6 +6,7 @@ import os
 import mock
 import sys
 
+import msmb_theme
 import sphinx_rtd_theme
 
 
@@ -79,8 +80,9 @@ linkcheck_ignore = [
     r'https://codecov.io/gh/MDAnalysis/mdacli/*',
     ]
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "msmb_theme"
+html_theme_path = [msmb_theme.get_html_theme_path(),
+                   sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
     'canonical_url': '',
     'logo_only': True,
@@ -106,6 +108,10 @@ html_static_path = ['_static']
 html_css_files = ['custom.css']
 html_use_opensearch = site_url
 
+html_context = {
+    'versions_json_url': os.path.join(site_url, "versions.json")
+}
+
 napoleon_use_ivar = True
 napoleon_use_rtype = False
 napoleon_use_param = False
@@ -113,5 +119,5 @@ napoleon_use_param = False
 # Configuration for intersphinx: refer to the Python standard library
 # and other packages used by mdacli
 intersphinx_mapping = {'https://docs.python.org/': None,
-                       'https://www.mdanalysis.org/docs/': None,
+                       'https://docs.mdanalysis.org/stable/': None,
                        }
