ce3a2926333da376c49914751be456851a588301
joaomcteixeira <joaomcteixeira@users.noreply.github.com>
2022-04-12 11:57:09 +0000

    [SKIP] version bump 0.1.17 -> 0.1.18
    Merge pull request #97 from hejamu/fix-typo
    
    Typo in libcli.py

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -62,7 +62,7 @@ project = 'mdacli'
 year = '2021'
 author = 'Philip Loche, Joao MC Teixeira and Oliver Beckstein'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.17'
+version = release = '0.1.18'
 
 pygments_style = 'trac'
 templates_path = ['_templates']
--- src/mdacli/__init__.py
+++ src/mdacli/__init__.py
@@ -11,4 +11,4 @@ from mdacli.cli import cli
 
 __all__ = ["cli"]
 
-__version__ = '0.1.17'
+__version__ = '0.1.18'
