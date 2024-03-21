37f4e27028348a89476a07a83e7b894622630832
hejamu <info@jaegerhenrik.de>
2024-03-21 15:56:23 +0100

    Add test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -36,6 +36,12 @@ def test_case_insensitive(args):
     """Test for beeing case insensitive."""
     subprocess.check_call(['mda', args, "-h"])
 
+@pytest.mark.parametrize('args', ("RMSF", "rmsf"))
+def test_case_insensitive_with_flags(args):
+    """Test for module name being case insensitive with additional flags."""
+    # Check if it still works if the module name is not the second argument
+    subprocess.check_call(['mda', '--debug', args, "-h"])
+
 
 def test_running_analysis(tmpdir):
     """Test running a complete analysis."""
