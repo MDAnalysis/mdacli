3e0638ae830d644a941c7fd576df222c348bfa30
hejamu <info@jaegerhenrik.de>
2024-04-03 15:49:41 +0200

    check even in detached state

--- devtools/check_changelog.py
+++ devtools/check_changelog.py
@@ -23,7 +23,10 @@ contributing = Path("docs", "CONTRIBUTING.rst")
 
 repo = git.Repo(folder)
 
-if repo.active_branch.name == "main":
+# Check if the developer is on the main branch
+# We check if the head commit is the same as the main branch head commit
+# as the CI will be in detached head state
+if repo.head.commit == repo.branches["main"].commit:
     print("You are on the main branch. Nothing to check.")
     exit(0)
 
@@ -32,8 +35,8 @@ with open(Path(folder, changelog), "r") as fin:
         if line.startswith("v"):
             raise ChangelogError(
                 "You have not updated the CHANGELOG file. "
-                f"Please add a summary of your additions to {str(changelog)!r} "
-                f"as described in {str(contributing)!r}."
+                f"Please add a summary of your additions to {str(changelog)!r}"
+                f" as described in {str(contributing)!r}."
             )
         elif line.startswith("*"):
             print("Changelog updated.")
