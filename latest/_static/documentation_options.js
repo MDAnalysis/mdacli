f66d070625bacc215e1e2ab6066e722031065fca
PicoCentauri <PicoCentauri@users.noreply.github.com>
2025-10-20 14:52:05 +0000

    deploy: d5917df7054ed6ab7dea317da8951843f886b0e7

---
+++ latest/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.33.dev5+gd5917df70',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
